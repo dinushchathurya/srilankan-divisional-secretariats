5e0b13ce3c2fd4a7f86a151ec04076095e2c1d0d
dinushchathurya <dinushchathurya21@gmail.com>
2021-07-03 23:08:11 +0530

    :sparkles: feat: add divisional secretariat upto kurunegala

---
+++ src/Secretariat.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Dinushchathurya\Secretariat;
+
+class Secretariat{
+	
+    public static $Secretariat = array(
+        
+        'Western' => array(
+            'Colombo' =>
+                array(
+                    'Colombo Divisional Secretariat',
+                    'Padukka Divisional Secretariat',
+                    'Seethawaka Divisional Secretariat',
+                    'Homagama Divisional Secretariat',
+                    'Thimbirigasyaya Divisional Secretariat',
+                    'Maharagama Divisional Secretariat',
+                    'Kaduwela Divisional Secretariat',
+                    'Ratmalana Divisional Secretariat',
+                    'Kesbewa Divisional Secretariat',
+                    'Sri Jayawardanapura Kotte Divisional Secretariat',
+                    'Dehiwala Divisional Secretariat',
+                    'Moratuwa Divisional Secretariat',
+                    'Hanwella Divisional Secretariat'
+                ),
+
+            'Gampaha' =>
+                array(
+                    'Negombo Divisional Secretariat',
+                    'Gampaha Divisional Secretariat',
+                    'Minuwangoda Divisional Secretariat',
+                    'Attanagalla Divisional Secretariat',
+                    'Biyagama Divisional Secretariat',
+                    'Divulapitiya Divisional Secretariat',
+                    'Dompe Divisional Secretariat',
+                    'Ja-ela Divisional Secretariat',
+                    'Katana Divisional Secretariat',
+                    'Kelaniya Divisional Secretariat',
+                    'Mahara Divisional Secretariat',
+                    'Meerigama Divisional Secretariat',
+                    'Wattala Divisional Secretariat',
+                ),
+
+            'Kalutara' =>
+                array(
+                    'Beruwala Divisional Secretariat',
+                    'Agalawatta Divisional Secretariat',
+                    'Bandaragama Divisional Secretariat',
+                    'Millaniya Divisional Secretariat',
+                    'Bulathsinhala Divisional Secretariat',
+                    'Dodangoda Divisional Secretariat',
+                    'Horana Divisional Secretariat',
+                    'Kalutara Divisional Secretariat',
+                    'Madurawela Divisional Secretariat',
+                    'Ingiriya Divisional Secretariat',
+                    'Matugama Divisional Secretariat',
+                    'Palindanuwara Divisional Secretariat',
+                    'Panadura Divisional Secretariat',
+                    'Walallawita Divisional Secretariat',
+                ),
+        ),
+
+        'North Western' => array(
+            'Kurunegala' =>
+                array(
+                    'Alawwa Divisional Secretariat',
+                    'Ambanpola Divisional Secretariat',
+                    'Bingiriya Divisional Secretariat',
+                    'Ehatuwewa Divisional Secretariat',
+                    'Ganewatte Divisional Secretariat',
+                    'Galgamuwa Divisional Secretariat',
+                    'Giribawa Divisional Secretariat',
+                    'Ibbagamuwa Divisional Secretariat',
+                    'Kotawehara Divisional Secretariat',
+                    'Kobeigane Divisional Secretariat',
+                    'Kuliyapitiya Divisional Secretariat',
+                    'Kurunegala Divisional Secretariat',
+                    'Maho Divisional Secretariat',
+                    'Maspotha Divisional Secretariat',
+                    'Mallawapitiya Divisional Secretariat',
+                    'Mawathagama Divisional Secretariat',
+                    'Narammala Divisional Secretariat',
+                    'Nikaweratiya Divisional Secretariat',
+                    'Panduwasnuwara East Divisional Secretariat',
+                    'Panduwasnuwara West Divisional Secretariat',
+                    'Pannala Divisional Secretariat',
+                    'Polgahawela Divisional Secretariat',
+                    'Polpithigama Divisional Secretariat',
+                    'Rasnayakapure Divisional Secretariat',
+                    'Rideegama Divisional Secretariat',
+                    'Weerabugedara Divisional Secretariat',
+                    'Wariyapola Divisional Secretariat',
+                    'Udubaddawa Divisional Secretariat',
+                ),
+        )
+    );
+}
